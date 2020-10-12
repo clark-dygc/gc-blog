042721054f0e61e603c65ac0b287098cda8b5704
clark-dygc <1210037252@qq.com>
2020-10-12 21:23:26 +0800

    开启gzip

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
+const os = require('os')
+const CompressionPlugin = require('compression-webpack-plugin')
+
+const platform = os.platform()
+
 module.exports = {
   publicPath: '/',
-  outputDir: process.env.NODE_ENV === 'production' ? '/var/www/blog/client' : 'dist'
+  outputDir: (process.env.NODE_ENV === 'production' && platform !== 'win32') ? '/var/www/blog/client' : 'dist',
+  productionSourceMap: false,
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'production') {
+      return {
+        plugins: [
+          new CompressionPlugin({
+            filename: '[path].gz[query]',
+            algorithm: 'gzip',
+            test: /\.js$|\.html$\.css/,
+            threshold: 10240,
+            deleteOriginalAssets: false
+          })
+        ]
+      }
+    }
+  }
 }
