66517c07e79ad782f6a4350597fdfc55b5e0fc37
clark-dygc <1210037252@qq.com>
2020-10-12 22:12:33 +0800

    测试按需加载element-ui

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui'
+// import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from './router'
 import store from './store'
@@ -12,7 +12,47 @@ Vue.use(mavonEditor)
 
 import './permission'
 
-Vue.use(ElementUI)
+import {
+  Button, Input, Table, Form, FormItem, Menu, Submenu, MenuItem, Icon,
+  Aside, Main, Container, Link, Tooltip, Backtop, Divider, Card, Dialog,
+  TableColumn, Dropdown, DropdownItem, DropdownMenu, PageHeader, Loading,
+  Avatar, Col, Rate, Row, Radio, RadioGroup
+} from 'element-ui'
+import CollapseTransition from 'element-ui/lib/transitions/collapse-transition';
+
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Table)
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(Icon)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Container)
+Vue.use(Link)
+Vue.use(Tooltip)
+Vue.use(Backtop)
+Vue.use(Divider)
+Vue.use(Card)
+Vue.use(Dialog)
+Vue.use(TableColumn)
+Vue.use(Dropdown)
+Vue.use(DropdownItem)
+Vue.use(DropdownMenu)
+Vue.use(PageHeader)
+Vue.use(Loading)
+Vue.use(Avatar)
+Vue.use(Col)
+Vue.use(Row)
+Vue.use(Radio)
+Vue.use(RadioGroup)
+Vue.use(Rate)
+Vue.component(CollapseTransition.name, CollapseTransition)
+
+// Vue.use(ElementUI)
 Vue.mixin(mixin)
 
 Vue.config.productionTip = false
